6588f413473b01547baf902fbf254065929e7214
Mehmet Ali <kemalist611661@gmail.com>
2022-10-19 22:56:42 +0300

    Palindrom kelime bulan program tamamlandı

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+
+static boolean isPalindrome(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+                return true;
+        else
+                return false;
+        }
+
+public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+
+    }
+}
